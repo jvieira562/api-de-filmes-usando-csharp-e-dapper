779d5e9dc554c9306759a4826b0339f38187dd93
Jose Victor Santos Vieira <jvieira@vunesp.com.br>
2022-11-10 17:57:54 -0300

    Iniciando projeto FilmesAPI

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,49 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int _Id = 1;
+
+        [HttpPost]
+        public IActionResult  AdicionaFilme([FromBody] Filme filme)
+        {
+
+            if (!string.IsNullOrEmpty(filme.Diretor))
+            {
+                filme.Id = _Id++;
+                filmes.Add(filme);
+                filme.ToString();
+                return CreatedAtAction(nameof(pergarFilmeAtravesDoId), new { Id = filme.Id }, filme);
+            }
+            else
+            {
+                Console.WriteLine("FILME NÃO É VALIDO!");
+                return BadRequest();
+            }
+
+        }
+        [HttpGet]
+        public IActionResult BuscarFilmes()
+        {
+            return Ok(filmes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult pergarFilmeAtravesDoId(int id)
+        {
+       
+            Filme filme =  filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound("Filme não encontrado.");
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [Required(ErrorMessage = "O campo gênero é obrigatório")]
+        [StringLength(30, ErrorMessage = "O gênero deve ter no máximo 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(100, 500, ErrorMessage = "A duração em minutos deve está entre 1 e 600")]
+        public int Duracao { get; set; }
+
+        public void ToString()
+    {
+            Console.WriteLine($"\nTítulo : {this.Titulo} \nDiretor : {this.Diretor} \nGênero : {this.Genero} \nDuração : {this.Duracao} minutos");
+    }
+    }
+
+}
